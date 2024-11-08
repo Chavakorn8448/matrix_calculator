f3dffb6c4a4c8027f3cfcd659d1b2e08677caf15
Copter1246 <kasidith.copter@gmail.com>
2024-11-08 16:49:38 +0700

    Update matrix_cal.py

--- matrix_cal.py
+++ matrix_cal.py
@@ -1,4 +1,29 @@
 def main():
+  
+  print("Choose the operation:")
+  print("   1 - Addition")
+  print("   2 - Subtraction")
+  print("   3 - Multiplication")
+  print("   4 - Finding determinants")
+  print("   5 - Transpose")
+  print("   6 - Inverse")
+  
+  choice = input("Enter your choice: ")
+  
+  if choice == '1':
+    pass
+  elif choice == '2':
+    pass
+  elif choice == '3':
+    pass
+  elif choice == '4':
+    pass
+  elif choice == '5':
+    pass
+  elif choice == '6':
+    pass
+  else:
+    print("Invalid choice")
   pass
 
 if __name__ == "__main__":

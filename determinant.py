cb58cd1ccef6a98a1cdb5d20b63ff8bd93dea654
Natcha Soranathavornkul <79828995+Natchasor@users.noreply.github.com>
2024-11-09 15:09:13 +0700

    Determinant function

---
+++ determinant.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def determinant(matrix):
+    return np.linalg.det(matrix)
+
+a = np.array([
+    [7, 4, 7],
+    [7, 5, 2],
+    [9, 1, 3],
+    
+])
+
+print(determinant(a))
+
+

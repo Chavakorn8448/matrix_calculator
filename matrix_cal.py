c2d49dc708523538370dff453fb82e42e324109c
UJYoYo <ujbaobao9899@gmail.com>
2024-11-09 17:44:03 +0700

    add and subtract + import all current functions to main

--- matrix_cal.py
+++ matrix_cal.py
@@ -1,5 +1,8 @@
 import numpy as np
-from Input import *
+from input_matrix import get_one_metrix, get_two_metrix, print_metrix
+from add_subtract_matrix_calc import add_matrices, subtract_matrices
+from transpose_matrix_calc import transpose_matrix
+from determinant import determinant
 
 def main():
     print("Choose the operation:")
@@ -9,12 +12,23 @@ def main():
     print("   4 - Finding determinants")
     print("   5 - Transpose")
     print("   6 - Inverse")
+    print("   7 - Exit")
     
     choice = input("Enter your choice: ")
     
     if choice == '1':
+        matrix1, matrix2 = get_two_metrix()
+        result = add_matrices(matrix1, matrix2)
+        if result is not None:
+            print("Sum:")
+            print_metrix(result)
         pass
     elif choice == '2':
+        matrix1, matrix2 = get_two_metrix()
+        result = subtract_matrices(matrix1, matrix2)
+        if result is not None:
+            print("Differences:")
+            print_metrix(result)
         pass
     elif choice == '3':
         pass
@@ -24,9 +38,11 @@ def main():
         pass
     elif choice == '6':
         pass
+    elif choice == '7':
+        return
     else:
         print("Invalid choice")
-    pass
+        pass
 
 if __name__ == "__main__":
     main()

e6e96baef813e3d2f8c245b48dffe423fde7ad19
Natcha Soranathavornkul <79828995+Natchasor@users.noreply.github.com>
2024-11-17 17:30:12 +0700

    delete test matrix

--- determinant.py
+++ determinant.py
@@ -3,13 +3,6 @@ import numpy as np
 def determinant(matrix):
     return np.linalg.det(matrix)
 
-a = np.array([
-    [7, 4, 7],
-    [7, 5, 2],
-    [9, 1, 3],
-    
-])
 
-print(determinant(a))
 
 
